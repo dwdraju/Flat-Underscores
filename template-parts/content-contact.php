791c865a14171389ddaaa12e4a9bf067b6cb8c71
linux <rajudawadinp@gmail.com>
2015-11-29 23:37:22 +0545

    Contact form 7 integrated

--- template-parts/content-contact.php
+++ template-parts/content-contact.php
@@ -5,14 +5,28 @@
 * @link https://codex.wordpress.org/Template_Hierarchy
 *
 * @package Flat_Underscores
+*
+* Contact form 7 installation required Link: https://wordpress.org/plugins/contact-form-7/
 */
+?>
+<?php
+	// Advanced Custom Field - Contact us
+$contact_label			=	get_field('contact_us_label');
+$contact_desc_label 	=	get_field('contact_us_desc_label'); 
+$google_map				=	get_field('google_map_location');
 ?>
 <section id="title" class="emerald">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-6">
+				<?php if($contact_label){?>
+				<h1><?php echo $contact_label;?></h1>
+				<?php } else{ ?>
 				<h1>Contact Us</h1>
-				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada</p>
+				<?php }?>
+				<?php if($contact_desc_label):?>
+					<p><?php echo $contact_desc_label;?></p>
+				<?php endif;?>
 			</div>
 			<div class="col-sm-6">
 				<ul class="breadcrumb pull-right">
@@ -29,31 +43,36 @@
 		<div class="col-sm-8">
 			<h4>Contact Form</h4>
 			<div class="status alert alert-success" style="display: none"></div>
+			<?php the_content();?>
+			<!-- To contact form 7 
 			<form id="main-contact-form" class="contact-form" name="contact-form" method="post" action="sendemail.php" role="form">
 				<div class="row">
 					<div class="col-sm-5">
 						<div class="form-group">
-							<input type="text" class="form-control" required="required" placeholder="First Name">
+							[text* your-first-name class:form-control placeholder "First Name"]
 						</div>
 						<div class="form-group">
-							<input type="text" class="form-control" required="required" placeholder="Last Name">
+							[text* your-last-name class:form-control placeholder "Last Name"]
 						</div>
 						<div class="form-group">
-							<input type="text" class="form-control" required="required" placeholder="Email address">
+							[email* your-email class:form-control placeholder "Email Address"]
 						</div>
 						<div class="form-group">
-							<button type="submit" class="btn btn-primary btn-lg">Send Message</button>
+							[submit class:btn class:btn-primary class:btn-lg "Send Message"]
 						</div>
 					</div>
 					<div class="col-sm-7">
-						<textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Message"></textarea>
+						[textarea* your-message class:form-control id:message *8 placeholder "Message"]
 					</div>
 				</div>
-			</form>
+			</form>  -->
+			
 		</div><!--/.col-sm-8-->
 		<div class="col-sm-4">
-			<h4>Our Location</h4>
-			<iframe width="100%" height="215" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.au/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Dhaka,+Dhaka+Division,+Bangladesh&amp;aq=0&amp;oq=dhaka+ban&amp;sll=40.714353,-74.005973&amp;sspn=0.836898,1.815491&amp;ie=UTF8&amp;hq=&amp;hnear=Dhaka+Division,+Bangladesh&amp;t=m&amp;ll=24.542126,90.293884&amp;spn=0.124922,0.411301&amp;z=8&amp;output=embed"></iframe>
+			<?php if($google_map):?>
+				<h4>Our Location</h4>
+				<iframe width="100%" height="215" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="<?php echo $google_map;?>"></iframe>
+			<?php endif;?>
 		</div><!--/.col-sm-4-->
 	</div>
     </section><!--/#contact-page-->
